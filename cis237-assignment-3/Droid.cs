35d14a2374173138cff7f0cb0f768ecf99b5901a
Cayden Greer <caydengreer@gmail.com>
2022-10-12 12:38:16 -0400

    Started laying the foundation for the Droid parent class as well as started laying out the utility class.

---
+++ cis237-assignment-3/Droid.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cis237_assignment_3
+{
+    internal class Droid
+    {
+        //--------------------------
+        // Constants
+        //VVVVVVVVVVVVVVVVVVVVVVVVVV
+
+
+        //--------------------------
+        // Variables
+        //VVVVVVVVVVVVVVVVVVVVVVVVVV
+
+        protected string material;
+        protected string color;
+        protected decimal totalCost;
+
+        //--------------------------
+        // Constructors
+        //VVVVVVVVVVVVVVVVVVVVVVVVVV
+
+        public Droid(string Material, string Color)
+        {
+            this.material = Material;
+            this.color = Color;
+        }
+
+        //--------------------------
+        // Properties
+        //VVVVVVVVVVVVVVVVVVVVVVVVVV
+
+        private decimal TotalCost
+        {
+            get { return totalCost; }
+            set { totalCost = value; }
+        }
+
+
+        //--------------------------
+        // Public Methods
+        //VVVVVVVVVVVVVVVVVVVVVVVVVV
+
+        public override string ToString()
+        {
+            return material + " " + color + " " + totalCost;
+        }
+
+        public decimal CalculateTotalCost()
+        {
+            return totalCost;
+        }
+
+        //--------------------------
+        // Protected Methods
+        //VVVVVVVVVVVVVVVVVVVVVVVVVV
+    }
+}
---
+++ cis237-assignment-3/Utility.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cis237_assignment_3
+{
+    internal class Utility : Droid
+    {
+        //--------------------------
+        // Variables
+        //VVVVVVVVVVVVVVVVVVVVVVVVVV
+
+        private bool toolbox;
+        private bool computerConnection;
+        private bool scanner;
+
+
+        //--------------------------
+        // Constructors
+        //VVVVVVVVVVVVVVVVVVVVVVVVVV
+
+        public Utility(string Material, string Color, bool ToolBox, bool ComputerConnection, bool Scanner) : base(Material, Color)
+        {
+            this.toolbox = ToolBox;
+            this.computerConnection = ComputerConnection;
+            this.scanner = Scanner;
+            
+        }
+
+        public Utility()
+        {
+
+        }
+
+
+
+
+        //--------------------------
+        // Public Methods
+        //VVVVVVVVVVVVVVVVVVVVVVVVVV
+
+
+    }
+}
